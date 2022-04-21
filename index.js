488c8c6e070e113d5bd1733766efa6b29c570c8c
jihye <wisdom_u@naver.com>
2022-04-21 20:14:50 +0900

    오류찾기위한 커밋

--- index.js
+++ index.js
@@ -67,13 +67,23 @@ app.post('/login', (req, res) => {
 })
 
 app.get('/api/users/auth', auth, (req, res) => {
+  //여기 까지 미들웨어를 통과해 왔다는 얘기는  Authentication 이 True 라는 말.
   res.status(200).json({
     _id: req.user._id,
     isAdmin: req.user.role === 0 ? false : true,
     isAuth: true,
-    email: req.user.lastname,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
     role: req.user.role,
     image: req.user.image,
+  })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: '' }, (err, user) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).send({ success: true })
   })
 })
 
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,19 @@
 const { User } = require('../models/User')
 
 let auth = (req, res, next) => {
-  //인증처리 하는 곳
+  //인증 처리를 하는곳
+  //클라이언트 쿠키에서 토큰을 가져온다.
 
-  //클라이언트 쿠키에서 토큰 가져오기
   let token = req.cookies.x_auth
-
-  //토큰 복호화 한 후 유저 찾기
-  User.findByToken(token, (err, cb) => {
+  // 토큰을 복호화 한후  유저를 찾는다.
+  User.findByToken(token, (err, user) => {
     if (err) throw err
     if (!user) return res.json({ isAuth: false, error: true })
+
     req.token = token
     req.user = user
     next()
   })
-
-  //유저 있으면 인증 OK
-  //유저 없으면 인증 NO
 }
 
 module.exports = { auth }
